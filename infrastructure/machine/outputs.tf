0cc59f8022748e0caba72eb83ffe2f60d9ed6e34
Nate Gay <email@nategay.me>
2020-03-02 22:27:14 -0600

    Resolves empty tuple error on machine module output

--- infrastructure/machine/outputs.tf
+++ infrastructure/machine/outputs.tf
@@ -1,7 +1,7 @@
 output "workspace_ipv4" {
-  value = aws_instance.workspace[0].public_ip
+  value = var.workspace_enabled ? aws_instance.workspace[0].public_ip : ""
 }
 
 output "workspace_ipv6" {
-  value = aws_instance.workspace[0].ipv6_addresses[0]
+  value = var.workspace_enabled ? aws_instance.workspace[0].ipv6_addresses[0] : ""
 }
