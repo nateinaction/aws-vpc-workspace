e83b0f2e36bc67fe2d19c23e3f76969d0265e391
Nate Gay <email@nategay.me>
2020-03-21 01:31:56 -0500

    Fix local repo path

--- image/files/etc/skel/workspace_setup/setup_workspace_repo.sh
+++ image/files/etc/skel/workspace_setup/setup_workspace_repo.sh
@@ -3,7 +3,7 @@
 REPO_NAME="workspace"
 REPO_OWNER="nateinaction"
 REPO_URL="git@github.com:${REPO_OWNER}/${REPO_NAME}.git"
-REPO_LOCAL_PATH="${HOME}/code/github.com/${REPO_OWNER}"
+REPO_LOCAL_PATH="${HOME}/code/github.com/${REPO_OWNER}/${REPO_NAME}"
 
 mkdir -p "${REPO_LOCAL_PATH}"
 git clone "${REPO_URL}" "${REPO_LOCAL_PATH}"
